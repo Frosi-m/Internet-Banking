7630cc8484a383ff048f55ec21d5ebaaf42b940e
Frosi-m <fahrurrosi380@gmail.com>
2022-11-11 01:41:52 -0800

    Create home.php
    yang home.php dan folder navigation cuma rancangannya aja
    masih belum komplit
    
    Co-Authored-By: fadil_abdi <96219808+fadil1302@users.noreply.github.com>

---
+++ home.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="POST"></form>
+    <h1>admin</h1>
+    <ul>
+        <li><a href="home.php">home</a></li>
+        <li><a href="home.php?link=register">register</a></li>
+        <li><a href="home.php?link=daftar_akun">daftar</a></li>
+        <li><a href="home.php?link=tambah_data">tambah_data</a></li>
+        <li><a href="home.php?link=perbarui_akun">perbarui akun</a></li>
+    </ul>
+
+    <h1>customer</h1>
+    <ul>
+        <li><a href="home.php">home</a></li>
+        <li><a href="home.php?link=lihat_transaksi">lihat transaksi</a></li>
+        <li><a href="home.php?link=melakukan_transaksi">transaksi</a></li>
+        <li><a href="home.php?link=edit_profile">edit profile</a></li>
+    </ul>
+    <?php
+    if (@$_GET[link] == 'register') {
+        include 'navigation/register.php';
+    }
+    else if(@$_GET[link] == 'daftar_akun'){
+        include 'navigation/daftar_akun.php';
+    }
+    else if(@$_GET[link] == 'perbarui_akun'){
+        include 'navigation/perbarui_akun.php';
+    }
+    else if(@$_GET[link] == 'tambah_data'){
+        include 'tambah_data.php';
+    }
+    else if(@$_GET[link] == 'lihat_transaksi'){
+        include 'lihat_transaksi.php';
+    }
+    else if(@$_GET[link] == 'melakukan_transaksi'){
+        include 'melakukan_transaksi.php';
+    }
+
+    else if(@$_GET[link] == 'edit_profile'){
+        include 'edit_profile.php';
+    }
+    ?>
+</body>
+</html>
